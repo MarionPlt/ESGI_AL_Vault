7f22b13a53f8e9e95b92a9df489662ac86413eb3
johan <johancampion13@gmail.com>
2022-07-14 19:12:42 +0200

    adding booklist widget

---
+++ FRONT/APP.Mobile/vaultapp/lib/app/screens/list/books_screens.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:vaultapp/app/modules/books/model/book.dart';
+import 'package:vaultapp/app/screens/list/list_element.dart';
+
+class BooksScreen extends StatelessWidget {
+  BooksScreen({Key? key}) : super(key: key);
+
+  List<Book> bookModel = [];
+  List<Map<String, dynamic>> bookList = [
+    {
+      "id": 1,
+      "title,": 'Les misérables',
+      "author": 'Victor Hugo',
+      "aquisitionDate,": '13-07-2020',
+      "parutionDate": '13/09-1862',
+      "imagePath,": '',
+      "state": 'Neuf'
+    },
+    {
+      "id": 2,
+      "title,": 'Les travailleur de la mer',
+      "author": 'Victor Hugo',
+      "aquisitionDate,": '13-07-2020',
+      "parutionDate": '13/09-1862',
+      "imagePath,": '',
+      "state": 'Abimé'
+    },
+
+  ];
+
+  @override
+  void initState() {
+    bookModel = bookList.map((e) => Book.fromJson(e)).toList();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: ListView(
+            children: [
+              Column(
+                children: bookModel.map((e) =>
+                    ListElement(title: e.title, author: e.author, aquisitionDate: e.aquisitionDate, parutionDate: e.parutionDate, state: e.state)).toList(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
