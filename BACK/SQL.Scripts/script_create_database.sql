40e2768d2897c639c58b97b3a36dcfaa09ccceeb
Julien POTY <jpoty@myges.fr>
2022-07-14 19:56:48 +0200

    add sql scripts

---
+++ BACK/SQL.Scripts/script_create_database.sql
@@ -0,0 +1,114 @@
+USE [master]
+GO
+
+CREATE DATABASE [vault_library]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+ --La valeur FILENAME correspond a l'emplacement du fichier de base de donnee qui va etre cree
+ --L'emplacement doit correpondre a "C:\chemin_vers_le_repertoire_installation_sql_server\MSSQL15.MSSQLSERVER\MSSQL\DATA\nom_de_la_bdd.mdf"
+ --"MSSQL15.MSSQLSERVER" correspond au repertoire de la version 2019 pour Sql Server
+( NAME = N'vault_library', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\vault_library.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'vault_library_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\vault_library_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [vault_library].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [vault_library] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [vault_library] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [vault_library] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [vault_library] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [vault_library] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [vault_library] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [vault_library] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [vault_library] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [vault_library] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [vault_library] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [vault_library] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [vault_library] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [vault_library] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [vault_library] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [vault_library] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [vault_library] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [vault_library] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [vault_library] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [vault_library] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [vault_library] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [vault_library] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [vault_library] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [vault_library] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [vault_library] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [vault_library] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [vault_library] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [vault_library] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [vault_library] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [vault_library] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [vault_library] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [vault_library] SET  READ_WRITE 
+GO
+
+
