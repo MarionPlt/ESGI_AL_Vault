91d6fd6c2a15e3f2077d58fddc88584a0306685b
Antoine CHATELARD <antoine.chatelard@acedigitalecom.onmicrosoft.com>
2022-07-15 18:45:52 +0200

    updated gitignore untrack appsettings.local

--- FRONT/APP.Mobile/vaultapp/lib/app/modules/items/data/providers/item_library_provider.dart
+++ FRONT/APP.Mobile/vaultapp/lib/app/modules/items/data/providers/item_library_provider.dart
@@ -1,13 +1,24 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:vaultapp/app/modules/items/data/models/book.dart';
 import 'package:vaultapp/app/modules/items/data/models/item.dart';
 
 class ItemLibraryProvider {
   final libraryURL = 'http://localhost:5194'; 
 
   Future<Item> getItemById(String itemId) async {
-    final result = await http.get(Uri.parse(libraryURL));
+    final result = await http.get(Uri.parse("$libraryURL/item/$itemId"));
     return Item.fromJson(jsonDecode(result.body));
+  }
+
+  Future<Book> createBook(Book book) async {
+    final response = await http.post(Uri.parse("$libraryURL/item/book"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: book.toJson());
+
+    if (response.)
   }
 }
