d8651709a78c33ddae6ae023791b56d713539f72
johan <johancampion13@gmail.com>
2022-07-23 09:20:53 +0200

    fix: scrolling issue in item list

--- FRONT/APP.Mobile/vaultapp/lib/app/screens/item_list/item_list_screen.dart
+++ FRONT/APP.Mobile/vaultapp/lib/app/screens/item_list/item_list_screen.dart
@@ -61,101 +61,101 @@ class _ItemListScreenState extends State<ItemListScreen> {
       body: BlocBuilder<ItemBloc, ItemState>(
         builder: (context, state) {
           if (state is GetAllItemsSuccessState) {
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  ExpansionPanelList(
-                    children: [
-                      ExpansionPanel(
-                          isExpanded: isExpanded,
-                          canTapOnHeader: true,
-                          headerBuilder: (context, isExpanded) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.only(left: 20, right: 20),
-                              child: Row(children: const [
-                                Icon(Icons.search),
-                                Text("Recherche")
-                              ]),
-                            );
-                          },
-                          body: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 20, right: 20, bottom: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    const Text("Type d'objet : "),
-                                    DropdownButton<String>(
-                                      style: const TextStyle(
-                                          fontSize: 15,
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold),
-                                      value: _typeController.text,
-                                      onChanged: (String? newValue) {
-                                        setState(() {
-                                          _typeController.text = newValue ?? "";
-                                        });
-                                      },
-                                      items: const [
-                                        DropdownMenuItem(
-                                          value: "",
-                                          child: Text(""),
-                                        ),
-                                        DropdownMenuItem(
-                                          value: "Movie",
-                                          child: Text("Film"),
-                                        ),
-                                        DropdownMenuItem(
-                                          value: "VideoGame",
-                                          child: Text("Jeu-vidéo"),
-                                        ),
-                                        DropdownMenuItem(
-                                          value: "Book",
-                                          child: Text("Livre"),
-                                        ),
-                                      ],
-                                    )
-                                  ],
+            return Column(
+              children: [
+                ExpansionPanelList(
+                  children: [
+                    ExpansionPanel(
+                        isExpanded: isExpanded,
+                        canTapOnHeader: true,
+                        headerBuilder: (context, isExpanded) {
+                          return Padding(
+                            padding:
+                                const EdgeInsets.only(left: 20, right: 20),
+                            child: Row(children: const [
+                              Icon(Icons.search),
+                              Text("Recherche")
+                            ]),
+                          );
+                        },
+                        body: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, bottom: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const Text("Type d'objet : "),
+                                  DropdownButton<String>(
+                                    style: const TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                    value: _typeController.text,
+                                    onChanged: (String? newValue) {
+                                      setState(() {
+                                        _typeController.text = newValue ?? "";
+                                      });
+                                    },
+                                    items: const [
+                                      DropdownMenuItem(
+                                        value: "",
+                                        child: Text(""),
+                                      ),
+                                      DropdownMenuItem(
+                                        value: "Movie",
+                                        child: Text("Film"),
+                                      ),
+                                      DropdownMenuItem(
+                                        value: "VideoGame",
+                                        child: Text("Jeu-vidéo"),
+                                      ),
+                                      DropdownMenuItem(
+                                        value: "Book",
+                                        child: Text("Livre"),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                width: 70.w,
+                                child: TextFormField(
+                                  decoration: const InputDecoration(
+                                      labelText: 'Libellé'),
+                                  controller: _labelController,
                                 ),
-                                SizedBox(
-                                  width: 70.w,
-                                  child: TextFormField(
-                                    decoration: const InputDecoration(
-                                        labelText: 'Libellé'),
-                                    controller: _labelController,
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Center(
-                                  child: TextButton(
-                                      onPressed: () {
-                                        _search();
-                                      },
-                                      child: const Text("Valider")),
-                                )
-                              ],
-                            ),
-                          ))
-                    ],
-                    expansionCallback: (int item, bool status) {
-                      setState(() {
-                        isExpanded = !isExpanded;
-                      });
-                    },
-                  ),
-                  ListView.builder(
+                              ),
+                              const SizedBox(height: 10),
+                              Center(
+                                child: TextButton(
+                                    onPressed: () {
+                                      _search();
+                                    },
+                                    child: const Text("Valider")),
+                              )
+                            ],
+                          ),
+                        ))
+                  ],
+                  expansionCallback: (int item, bool status) {
+                    setState(() {
+                      isExpanded = !isExpanded;
+                    });
+                  },
+                ),
+                Expanded(
+                  child: ListView.builder(
                       scrollDirection: Axis.vertical,
                       shrinkWrap: true,
                       itemCount: state.items.length,
                       itemBuilder: (context, index) {
                         return ItemTile(item: state.items[index]);
                       }),
-                  SizedBox(height: 5.h)
-                ],
-              ),
+                ),
+                SizedBox(height: 5.h)
+              ],
             );
           } else if (state is ItemLoadingState) {
             return const Center(child: CircularProgressIndicator());
